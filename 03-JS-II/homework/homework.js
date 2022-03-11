6422a377667e925c0c2f11633b0a4af31f691dd0
Agustin Viera <facundoviera98@gmail.com>
2022-03-11 13:49:37 -0300

    First commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,6 +1,6 @@
 // No cambies los nombres de las funciones.
 
-function obtenerMayor(x, y) {
+function "viera"(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
